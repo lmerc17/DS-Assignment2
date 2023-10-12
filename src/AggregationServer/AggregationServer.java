cd2a157b6cee31cce47d6f5ad5f6fdb1d3653145
lmerc17 <a1804049@adelaide.edu.au>
2023-10-13 00:54:25 +1030

    Aggregation Server can now ignore requests with invalid IDs and also delete weather data after a content server has been disconnected for 30 seconds

--- src/AggregationServer/AggregationServer.java
+++ src/AggregationServer/AggregationServer.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class AggregationServer {
 
@@ -53,13 +54,13 @@ public class AggregationServer {
         while(true) { //infinite loop to allow server to accept multiple clients
 
             // a try-catch statement to ensure all socket operation runs smoothly
-            try (Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-            ) {
+            try{
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
                 //a thread object is created to take in all the client information and location of initial weather data
-                Thread t = new ClientHandler(clientSocket, in, out, initialJsonWeatherData);
+                Thread t = new ClientHandler(clientSocket, in, out);
 
                 //thread is started
                 t.start();
@@ -95,7 +96,7 @@ class ClientHandler extends Thread{
 
     }
 
-    /** Method to save the jsonData to the weather files.
+    /** Method to save jsonData to the weather file.
      * @param  jsonData: A string containing all the jsonData sent by the content server.
      * @param destinationFile: The path to the file where the weather data is stored.
      */
@@ -130,9 +131,10 @@ class ClientHandler extends Thread{
 
         Scanner initialJson = new Scanner(initialJsonWeatherData); // to read from the current weather file
         PrintWriter newJson = new PrintWriter(newJsonWeatherData); // to write to the new weather file
-        String id = jsonData.substring(jsonData.indexOf("\"id\"") + 7, jsonData.indexOf("\"name\"")-4); // id of the data being added
+        String id = jsonData.substring(jsonData.indexOf("\"id\"") + 6, jsonData.indexOf("\"name\"")-4); // id of the data being added
         System.out.println("THIS IS THE ID: " + id);
         String line;
+        boolean dataAdded = false;
 
         while(initialJson.hasNextLine()){ // while there is still a line to be read in the current weather file
             line = initialJson.nextLine(); // read the line
@@ -143,11 +145,104 @@ class ClientHandler extends Thread{
                 newJson.print(jsonData.substring(2)); // add that jsonData to the new weather file (minus the starting curly bracket)
                 while(!line.contains("}")){ // while line does not contain the ending curly bracket
                     line = initialJson.nextLine(); // progress through the lines
+                    dataAdded = true;
                 }
             }
         }
+
+        if(!dataAdded){
+            newJson.print(jsonData);
+        }
+
         newJson.close(); //close the PrintWriter
         initialJson.close(); //close the scanner
+
+        // delete the initial weather data
+        if(!initialJsonWeatherData.delete()){
+            System.err.println("Original weather.txt could not be deleted");
+            System.exit(1);
+        }
+        // rename the new weather file to the same name as the initial one
+        if(!newJsonWeatherData.renameTo(initialJsonWeatherData)){
+            System.err.println("Temp weather file wasn't renamed");
+            System.exit(1);
+        }
+        // delete the backup weather file
+        if(!jsonWeatherDataBackup.delete()){
+            System.err.println("weather_backup.txt could not be deleted");
+            System.exit(1);
+        }
+
+    }
+
+    /** Method to delete certain jsonData from the weather file.
+     * @param  id: A string containing all the ID of the weather data to be deleted
+     * @param destinationFile: The path to the file where the weather data is being deleted.
+     */
+    private static void delete_data(String id, String destinationFile) throws IOException{
+
+        // initialJsonWeather Data and newJsonWeatherData files are created.
+        File jsonWeatherDataBackup = new File("AggregationServer/weather_backup.txt");
+        File initialJsonWeatherData = new File("AggregationServer/" + destinationFile);
+        File newJsonWeatherData = new File("AggregationServer/weather_temp.txt");
+
+        if(jsonWeatherDataBackup.exists()){ // if weather_backup.txt exists
+            if(!newJsonWeatherData.delete()){ // delete it
+                System.err.println("weather_backup.txt cannot be deleted");
+                System.exit(1);
+            }
+        }
+        if(newJsonWeatherData.exists()){ // if weather_temp.txt exists
+            if(!newJsonWeatherData.delete()){ // delete it
+                System.err.println("weather_temp.txt cannot be deleted");
+                System.exit(1);
+            }
+        }
+
+        // copy initial json weather data into backup file
+        Files.copy(initialJsonWeatherData.toPath(), jsonWeatherDataBackup.toPath());
+
+        //create weather_temp file
+        if(!newJsonWeatherData.createNewFile()){
+            System.err.println("weather_temp.txt could not be created");
+            System.exit(1);
+        }
+
+        Scanner initialJson = new Scanner(initialJsonWeatherData); // to read from the current weather file
+        PrintWriter newJson = new PrintWriter(newJsonWeatherData); // to write to the new weather file
+        String line;
+
+        System.out.println(id);
+
+        while(initialJson.hasNextLine()){ // while there is still a line to be read in the current weather file
+            line = initialJson.nextLine(); // read the line
+            if(line.contains("id") && !line.contains(id)){
+                newJson.println("{");
+                newJson.println(line);
+            }
+            else if(!line.contains(id) && !line.contains("{")){
+                newJson.println(line);
+            }
+            else{ // if the line does contain the id of the jsonData being deleted
+                while(!line.contains("}") && initialJson.hasNextLine()){ // skip to the next }
+                    line = initialJson.nextLine(); // progress through the lines and don't read them
+                }
+            }
+        }
+
+        newJson.close(); //close the PrintWriter
+        initialJson.close(); //close the scanner
+
+//        if(lineCount == 1){ // if only 1 ID was present in the file and it was deleted (there will be a leftover {)
+//            if(!newJsonWeatherData.delete()){ // to clear the newJsonWeatherFile, it will be deleted and created
+//                System.err.println("weather_backup.txt cannot be deleted");
+//                System.exit(1);
+//            }
+//            if(!newJsonWeatherData.createNewFile()){
+//                System.err.println("weather_backup.txt cannot be created");
+//                System.exit(1);
+//            }
+//        }
 
         // delete the initial weather data
         if(!initialJsonWeatherData.delete()){
@@ -208,30 +303,30 @@ class ClientHandler extends Thread{
     final BufferedReader in;
     final PrintWriter out;
 
-    File initialJsonWeatherData;
+    final File initialJsonWeatherData = new File("AggregationServer/weather.txt");
 
     /** Constructor for a client handler class
      * @param s: The clientSocket
      * @param in: The BufferedReader which is reading the GETClient information
      * @param out: The PrintWriter which is sending information to the GETClient
-     * @param initialJsonWeatherData: The file where the weather data is stored
      */
-    public ClientHandler(Socket s, BufferedReader in, PrintWriter out, File initialJsonWeatherData){
+    public ClientHandler(Socket s, BufferedReader in, PrintWriter out){
         clientSocket = s;
         this.in = in;
         this.out = out;
-        this.initialJsonWeatherData = initialJsonWeatherData;
     }
 
     /** Run method for the thread ClientHandler object. */
     @Override
     public void run(){
 
+        StringBuilder fullJsonData = null;
+        String destinationFile = null;
+
         try {
             // defining strings for later use
             String inputLine;
             String requestedData;
-            String destinationFile;
             String content_length;
             while ((inputLine = in.readLine()) != null) { // read in input
 
@@ -263,7 +358,7 @@ class ClientHandler extends Thread{
                         send_acknowledgement("204 NO_CONTENT", content_length, out); //send a 204 acknowledgment back to the content server
                     } else { // otherwise (content length isn't 0)
 
-                        StringBuilder fullJsonData = new StringBuilder();
+                        fullJsonData = new StringBuilder();
                         String status;
 
                         //if initial file is there set status as 200 otherwise set it as 201
@@ -274,14 +369,35 @@ class ClientHandler extends Thread{
                         }
 
                         try { // try to save data
-                            while ((inputLine = in.readLine()) != null) {
-                                fullJsonData.append(inputLine).append("\n");
-                                if (inputLine.contains("}")) {
+                            while ((inputLine = in.readLine()) != null) { //while there is data to read
+                                fullJsonData.append(inputLine).append("\n"); //append the input line to fullJsonData
+                                if (inputLine.contains("}")) { //if the input contains }, stop reading
                                     break;
+                                }
+                                else if (inputLine.contains("id")){ // if the input contains id:
+                                    if(inputLine.contains("IDS")) { // if the id contains IDS
+                                        try { // try turning the ID after it into a number
+                                            Integer.parseInt(inputLine.substring(10, inputLine.length()-2));
+                                        } catch (NumberFormatException e) { // if it is not a valid number, end the connection and stop the thread
+                                            System.err.println("Invalid ID in Data received");
+                                            clientSocket.close();
+                                            in.close();
+                                            out.close();
+                                            return; // close the thread
+                                        }
+                                    }
+                                    else{ // if IDS isn't present, end the connection and stop the thread.
+                                        System.err.println("Invalid ID in Data received");
+                                        clientSocket.close();
+                                        in.close();
+                                        out.close();
+                                        return;
+                                    }
                                 }
                             }
                             save_data(fullJsonData.toString(), destinationFile);
-                        } catch (IOException e) { // catch IOException if there are issues saving data
+                        }
+                        catch (IOException e) { // catch IOException if there are issues saving data
                             send_acknowledgement("500 INTERNAL_SERVER_ERROR", "0", out); // send bad json acknowledgment
                         }
 
@@ -300,8 +416,19 @@ class ClientHandler extends Thread{
             }
         }
         catch(IOException e){
-            System.err.println("IOException in ClientHandler");
-            System.exit(1);
+            if(fullJsonData != null && destinationFile != null) {
+                try {
+                    TimeUnit.SECONDS.sleep(30); //run the timer
+                    String jsonData = fullJsonData.toString();
+                    String id = jsonData.substring(jsonData.indexOf("\"id\"") + 6, jsonData.indexOf("\"name\"") - 4); // id of the data being added
+                    delete_data(id, destinationFile);
+                }
+                catch (InterruptedException | IOException ex) {
+                }
+
+            }
+
+
         }
 
     }
